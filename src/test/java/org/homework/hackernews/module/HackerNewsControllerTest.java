c16b1487157e2921bf9a56bf42160e601b07b5c2
simivalley <peopletruthbetold@gmail.com>
2019-05-14 10:59:29 -0700

    add mockito-core in pom.xml

--- src/test/java/org/homework/hackernews/module/HackerNewsControllerTest.java
+++ src/test/java/org/homework/hackernews/module/HackerNewsControllerTest.java
@@ -1,4 +1,22 @@
 package org.homework.hackernews.module;
 
+import org.junit.Before;
+
+import static org.mockito.Mockito.mock;
+
 public class HackerNewsControllerTest {
+
+    private NewsItemsBean newsItemsBean;
+    private HackerNewsController hackerNewsController;
+    private TopNewsUpdater topNewsUpdater;
+
+    @Before
+    public void setUp() throws Exception {
+        newsItemsBean = mock(NewsItemsBean.class);
+        topNewsUpdater = mock(TopNewsUpdater.class);
+        hackerNewsController = new HackerNewsController(newsItemsBean,topNewsUpdater);
+    }
+
+
+
 }
